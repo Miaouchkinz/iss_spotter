4998ce39445f3b860c0666f0fdefb939da1cb1f6
Gab Richard <g.elodierichard@gmail.com>
2020-01-18 02:38:59 +0000

    forgot to save changes on last push :)

--- iss.js
+++ iss.js
@@ -23,48 +23,52 @@ const nextISSTimesForMyLocation = function(callback) {
 };
 
 const fetchMyIP = function(callback) {
-  // use request to fetch IP address from JSON API
-  request(`https://api.ipify.org?format=json`, (error, response, body) => {
-    // inside the request callback ...
-    // error can be set if invalid domain, user is offline, etc.
-    if (error) {
-      return callback(error, null);
-    }
-    // if non-200 status, assume server error
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      return callback(Error(msg), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
     }
-    // if we get here, all's well and we got the datalet data = JSON.parse(body);
+
     const ip = JSON.parse(body).ip;
     callback(null, ip);
   });
 };
 
-// API Call #2: Fetch Geo Coordinates By IP
-const fetchCoordsByIp = function(ip, callback) {
+const fetchCoordsByIP = function(ip, callback) {
   request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
     if (error) {
-      return callback(error, null);
+      callback(error, null);
+      return;
     }
+
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching geo-coordinates. Response: ${body}`;
-      return callback(Error(msg), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
     }
+
     const { latitude, longitude } = JSON.parse(body).data;
+    // console.log('lat/lng data:', { latitude, longitude });
+
     callback(null, { latitude, longitude });
   });
 };
 
 const fetchISSFlyOverTimes = function(coords, callback) {
-  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
     if (error) {
-      return callback(error, null);
+      callback(error, null);
+      return;
     }
+
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching geo-coordinates. Response: ${body}`;
-      return callback(Error(msg), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
     }
+
     const passes = JSON.parse(body).response;
     callback(null, passes);
   });
