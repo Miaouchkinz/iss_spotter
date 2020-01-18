4ffafa82cb7bbf896bca73fce09ee84ab8a07e96
Gab Richard <g.elodierichard@gmail.com>
2020-01-18 02:29:18 +0000

    entered callback hell of iss api, moved over the functionalities to iss

---
+++ iss.js
@@ -0,0 +1,73 @@
+const request = require(`request`);
+
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
+  });
+};
+
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request(`https://api.ipify.org?format=json`, (error, response, body) => {
+    // inside the request callback ...
+    // error can be set if invalid domain, user is offline, etc.
+    if (error) {
+      return callback(error, null);
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+    // if we get here, all's well and we got the datalet data = JSON.parse(body);
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+  });
+};
+
+// API Call #2: Fetch Geo Coordinates By IP
+const fetchCoordsByIp = function(ip, callback) {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching geo-coordinates. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+    const { latitude, longitude } = JSON.parse(body).data;
+    callback(null, { latitude, longitude });
+  });
+};
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching geo-coordinates. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation };
